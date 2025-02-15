62e4444ea0337630659d052c474fd3e0f5f90247
Kaiya <kaiyakatz@gmail.com>
2025-02-15 11:14:06 -0500

    021525code that deosnt work

---
+++ Assets/Geo_Quest/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+string String = "Hello";
+int Bleh = 3;
+// Start is called before the first frame update
+void Start()
+    {
+      Debug.Log("Hello World");
+        string String2 = "world";
+    Debug.Log(String + String2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Bleh);
+    Debug.Log(3);
+ 
+        
+    }
+
